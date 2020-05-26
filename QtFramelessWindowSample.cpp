b25ad825f7273b8b49056dc158c77e844781ca80
winsoft666 <winsoft666@outlook.com>
2020-05-26 23:46:08 +0800

    Support Aero snap

--- QtFramelessWindowSample.cpp
+++ QtFramelessWindowSample.cpp
@@ -1,16 +1,13 @@
 #include "QtFramelessWindowSample.h"
 
 QtFramelessWindowSample::QtFramelessWindowSample(QWidget *parent)
-    : FramelessWindow(true, parent) // Titlebar widget has border radius so set translucentBackground true.
+    : FramelessWindow(true, false, true, parent) // Titlebar widget has border radius so set translucentBackground true.
 {
     ui.setupUi(this);
 
-    // Necessary
-    setAllWidgetMouseTracking(this);
-
     // Optional
     setResizeable(true);
-    setTitlebar({ ui.widgetTitlebar, ui.labelTitle });
+    setTitlebar({ ui.widgetTitlebar, ui.labelTitle }, {ui.pushButtonClose}, {});
 
 
     loadStyleSheetFile(":/QtFramelessWindowSample/Resources/main.css", this);
